a779827588c468bc0849598117a04b8ca8990bf7
haeeeun <hegfs6327@naver.com>
2020-12-16 17:29:05 +0900

    로그인, 회원가입, 인증까지 구현

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useDispatch} from "react-redux";
+import { loginUser } from '../../../_action/user_action'
+import { withRouter } from 'react-router-dom'
 
-function LoginPage(){
+function LoginPage(props){
+    const dispatch = useDispatch()
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault(); //이것을 안넣으면 새로고침 된다.
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if(response.payload.loginSuccess){
+                    props.history.push('/')
+                } else{
+                    alert('error')
+                }
+            })
+    }
+
     return (
-        <div>
-            LoginPage !
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'}}>
+            <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={onSubmitHandler}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler}/>
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler}/>
+                <br/>
+                <button type="submit">Login</button>
+            </form>
         </div>
     )
 }
 
-export default LoginPage
+export default withRouter(LoginPage)
